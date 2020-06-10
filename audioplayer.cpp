2f06890995d3bf4748c887e94c7a089ab632f430
Raphael64 <609307301@qq.com>
2020-06-10 22:30:32 +0800

    背景音乐的播放不需要依赖文件路径了 其他的音效还需调整

--- audioplayer.cpp
+++ audioplayer.cpp
@@ -2,29 +2,18 @@
 #include <QDir>
 #include <QMediaPlayer>
 #include <QMediaPlaylist>
-
-//经过实验，用常规方法做的循环音乐会导致卡顿，故使用该方法
-static const QString s_curDir = QDir::currentPath() + "\\";
+QString LOCATION ="E:\\Code\\C++2020\\TowerDefenseQt\\music\\"; //在次填写路径否则相关音效真的难顶
+//经过实验，用常规方法做的循环音乐会导致卡顿，故使用该方法。
+static const QString s_curDir = QDir::currentPath();
 QMediaPlayer *player =new QMediaPlayer;
 AudioPlayer::AudioPlayer(QObject *parent)
     : QObject(parent)
     , m_backgroundMusic(NULL)
 {
+    player->setMedia(QUrl("qrc:/music/Bgmusic.mp3"));
+    player->setVolume(30);
+    player->play();
     // 创建一直播放的背景音乐
-    QUrl backgroundMusicUrl = QUrl::fromLocalFile("E:\\Code\\C++2020\\TowerDefenseQt\\music\\Bgmusic.mp3");
-    if (QFile::exists(backgroundMusicUrl.toLocalFile()))
-    {
-        m_backgroundMusic = new QMediaPlayer(this);
-        QMediaPlaylist *backgroundMusicList = new QMediaPlaylist();
-
-        QMediaContent media(backgroundMusicUrl);
-        backgroundMusicList->addMedia(media);
-        backgroundMusicList->setCurrentIndex(0);
-        // 设置背景音乐循环播放
-        backgroundMusicList->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
-        m_backgroundMusic->setPlaylist(backgroundMusicList);
-        m_backgroundMusic->setVolume(0);
-    }
 
 }
 
@@ -38,17 +27,17 @@ void AudioPlayer::playSound(SoundType soundType)
 {
     static const QUrl mediasUrls[] =
     {
-        QUrl::fromLocalFile( "E:\\Code\\C++2020\\TowerDefenseQt\\music\\tower_place.wav"),
-        QUrl::fromLocalFile("E:\\Code\\C++2020\\TowerDefenseQt\\music\\life_lose.wav"),
-        QUrl::fromLocalFile("E:\\Code\\C++2020\\TowerDefenseQt\\music\\laser_shoot.wav"),
-        QUrl::fromLocalFile( "E:\\Code\\C++2020\\TowerDefenseQt\\music\\enemy_destroy.wav")
+        QUrl::fromLocalFile(LOCATION+"tower_place.wav"),
+        QUrl::fromLocalFile(LOCATION+"life_lose.wav"),
+        QUrl::fromLocalFile(LOCATION+"laser_shoot.wav"),
+        QUrl::fromLocalFile(LOCATION+"enemy_destroy.wav")
     };
     static QMediaPlayer player;
 
     if (QFile::exists(mediasUrls[soundType].toLocalFile()))
     {
         player.setMedia(mediasUrls[soundType]);
-        player.setVolume(0);
+        player.setVolume(10);
         player.play();
 
     }
